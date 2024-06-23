f50f2c66dde715a5eadb2f33fddc0b91227cdd02
andre <andrinho@gmail.com>
2024-06-23 15:58:11 -0300

    feat: output subnets

--- output.tf
+++ output.tf
@@ -1,6 +1,12 @@
 output "subnet_pub_1a" {
-  value = module.eks_network.subnet_pub_1a.id
+  value = module.eks_network.subnet_pub_1a
 }
 output "subnet_pub_1b" {
-  value = module.eks_network.subnet_pub_1b.id
+  value = module.eks_network.subnet_pub_1b
+}
+output "subnet_priv_1a" {
+  value = module.eks_network.eks_subnet_private_1a
+}
+output "subnet_priv_1b" {
+  value = module.eks_network.eks_subnet_private_1b
 }
