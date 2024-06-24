8574bceb09afad5bd50a331e8794395271200ffe
andre <andrinho@gmail.com>
2024-06-24 18:26:35 -0300

    feat: policy do controller adicionada

--- modules/cluster/output.tf
+++ modules/cluster/output.tf
@@ -1,6 +1,7 @@
 output "eks_vpc_config" {
   value = aws_eks_cluster.eks_cluster.vpc_config
 }
-output "oidc" {
-  value = data.tls_certificate.eks_oidc_tls_certificate.certificates[0].sha1_fingerprint
+
+output "cluster_name" {
+  value = aws_eks_cluster.eks_cluster.id
 }
--- output.tf
+++ output.tf
@@ -13,7 +13,3 @@ output "subnet_priv_1b" {
 output "eks_vpc_config" {
   value = module.eks_cluster.eks_vpc_config
 }
-
-output "oidc" {
-  value = module.eks_cluster.oidc
-}
