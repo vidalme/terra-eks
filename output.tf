dad7e0c644e93fd39750dda20387357777f28fad
andre <andrinho@gmail.com>
2024-06-25 15:25:26 -0300

    feat: uma ruma de coisa

--- output.tf
+++ output.tf
@@ -1,15 +1,27 @@
-output "subnet_pub_1a" {
-  value = module.eks_network.subnet_pub_1a
-}
-output "subnet_pub_1b" {
-  value = module.eks_network.subnet_pub_1b
-}
-output "subnet_priv_1a" {
-  value = module.eks_network.subnet_priv_1a
-}
-output "subnet_priv_1b" {
-  value = module.eks_network.subnet_priv_1b
-}
-output "eks_vpc_config" {
-  value = module.eks_cluster.eks_vpc_config
-}
+# output "subnet_pub_1a" {
+#   value = module.eks_network.subnet_pub_1a
+# }
+# output "subnet_pub_1b" {
+#   value = module.eks_network.subnet_pub_1b
+# }
+# output "subnet_priv_1a" {
+#   value = module.eks_network.subnet_priv_1a
+# }
+# output "subnet_priv_1b" {
+#   value = module.eks_network.subnet_priv_1b
+# }
+# output "eks_vpc_config" {
+#   value = module.eks_cluster.eks_vpc_config
+# }
+
+# output "oidc" {
+#   value = module.eks_cluster.oidc
+# }
+
+# output "certificate_authority" {
+#   value = module.eks_cluster.certificate_authority
+# }
+
+# output "endpoint" {
+#   value = module.eks_cluster.endpoint
+# }
--- provider.tf
+++ provider.tf
@@ -4,14 +4,45 @@ terraform {
       source  = "hashicorp/aws"
       version = "5.55.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.31.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.14.0"
+    }
   }
+
   backend "s3" {
     bucket = "comunidadedevops-fortaleza"
     key    = "dev/terraform.tfstate"
     region = "us-east-1"
   }
+
 }
 
 provider "aws" {
   region = "us-east-1"
+}
+
+provider "kubernetes" {
+  host                   = module.eks_cluster.endpoint
+  cluster_ca_certificate = base64decode(module.eks_cluster.certificate_authority)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
+    command     = "aws"
+  }
+}
+provider "helm" {
+  kubernetes {
+    host                   = module.eks_cluster.endpoint
+    cluster_ca_certificate = base64decode(module.eks_cluster.certificate_authority)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
+      command     = "aws"
+    }
+  }
 }
